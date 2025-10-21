1b1accdd9dea4b104c70171203a2dd5fd7af01cf
ByteMe-404 <rian.official.444@gmail.com>
2025-10-21 11:31:12 +0600

    #

---
+++ js/eye-on-off.js
@@ -0,0 +1,15 @@
+const passwordInput = document.getElementById("password");
+const togglePassword = document.getElementById("togglePassword");
+
+if (togglePassword && passwordInput) {
+  togglePassword.addEventListener("click", () => {
+    const type =
+      passwordInput.getAttribute("type") === "password" ? "text" : "password";
+    passwordInput.setAttribute("type", type);
+
+    togglePassword.src =
+      type === "password"
+        ? "/assets/icons/eye-off.png"
+        : "/assets/icons/eye-on.png";
+  });
+}
---
+++ js/main.js
@@ -0,0 +1,12 @@
+function loadScript(path) {
+  const script = document.createElement("script");
+  script.src = path;
+  script.defer = true;
+  document.body.appendChild(script);
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+
+  loadScript("../js/eye-on-off.js");
+
+});
