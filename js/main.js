db33c6a19260f1e364805821d4877f1e93b6875d
ByteMe-404 <rian.official.444@gmail.com>
2025-10-23 08:19:32 +0600

    #

--- js/main.js
+++ js/main.js
@@ -10,3 +10,32 @@ document.addEventListener("DOMContentLoaded", () => {
   loadScript("../js/eye-on-off.js");
 
 });
+
+
+
+
+
+
+
+(function addGlobalLoader() {
+  // Add loader CSS link dynamically
+  const cssLink = document.createElement("link");
+  cssLink.rel = "stylesheet";
+  cssLink.href = "../css/styles.css";
+  document.head.appendChild(cssLink);
+
+  // Add loader HTML dynamically
+  const loaderHTML = `
+    <div id="loader">
+      <div class="spinner"></div>
+    </div>
+  `;
+  document.body.insertAdjacentHTML("afterbegin", loaderHTML);
+
+  // Fade out on load
+  window.addEventListener("load", () => {
+    const loader = document.getElementById("loader");
+    loader.style.opacity = "0";
+    setTimeout(() => loader.remove(), 100);
+  });
+})();
